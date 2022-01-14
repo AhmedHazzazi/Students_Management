d31d3e96515d0b04867379c8327efb302b114968
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2022-01-14 11:08:33 +0300

    my first commit

--- app.js
+++ app.js
@@ -14,4 +14,11 @@ mongoose.connect('mongodb://localhost:27017/Students_DB', {useNewUrlParser: true
 app.use('/', router);
 
 // Port
-app.listen(3000, ()=>console.log('express started!'));
+// app.listen(3000, ()=>console.log('express started!'));
+// if(process.env.NODE_ENV === "test") app.set("port", 3001);
+// else app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
+const server = app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:
+    ${app.get("port")}`);
+});
